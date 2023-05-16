ff29299789e550106e5080f2ffd5b86c49ad00af
Goodness Okanlawon <102490495+CodeEvolver@users.noreply.github.com>
2023-05-16 10:26:10 +0100

    enhanced ux

--- src/Components/main.js
+++ src/Components/main.js
@@ -16,51 +16,40 @@ function Main({theme}) {
         setMarkdown(modifiedMarkdown);
     }
     var style = {
-        backgroundColor: "#EAE8F6",
-        height: "90%",
-    }
-    var darkStyle = {
-        backgroundColor: "#000020",
+        backgroundColor: theme==="light"? "#EAE8F6": "#000020",
         height: "90%",
     }
     var editorPrevStyle = {
         height: "75%",
-        backgroundColor: "#F5F5F5",
-        color: "#1A237E"
-    }
-    var darkEditorPrevStyle = {
-        height: "75%",
-        backgroundColor: "#00002F",
-        color: "#EAE8F6",
+        backgroundColor: theme==="light"? "#F5F5F5": "#00002F",
+        color: theme==="light"? "#1A237E": "#EAE8F6",
+        border: theme==="dark" && "2px solid #EAE8F6",
     }
     var headerStyle = {
-        color: "#1A237E",
+        color: theme==="light"? "#1A237E": "#EAE8F6",
         fontWeight: "500",
         height: "",
-        backgroundColor: "#F5F5F5",
+        backgroundColor: theme==="light"? "#F5F5F5": "#00002F",
+        border: theme==="dark" && "2px solid #EAE8F6",
     }
-    var darkHeaderStyle = {
-        color: "#EAE8F6",
-        fontWeight: "500",
-        height: "",
-        backgroundColor: "#00002F",
+    var editorStyle = {
+        color: theme==="light"? "#1A237E": "#EAE8F6",
+        resize: "horizontal",
+        overflow:"auto",
     }
-    var editorLightText = {
-        color: "#1A237E",
-    }
-    var editorDarkText = {
-        color: "#EAE8F6",
+    var prevStyle = {
+        overflow:"auto"
     }
     
     return (
-        <div className="app-main w-100 px-sm-5 px-2 h-100 d-flex flex-column justify-content-center " style={theme === "light"?style: darkStyle}>
-            <div className="w-100 shadow d-flex flex-row rounded-top align-items-center justify-content-between px-3" style={theme==="light"?headerStyle: darkHeaderStyle}>
+        <div className="app-main w-100 px-sm-5 px-2 h-100 d-flex flex-column justify-content-center " style={style}>
+            <div className="w-100 shadow d-flex flex-row rounded-top align-items-center justify-content-between px-3" style={headerStyle}>
                 <p  className="border-0">Edit</p>
                 <p className="border-0">Preview</p>
             </div>
-            <div className="w-100 m-0 d-flex flex-row shadow rounded-bottom" style={theme==="light"?editorPrevStyle: darkEditorPrevStyle}> 
-                <textarea id="editor" className="textarea w-50 h-100 border-0 bg-transparent border-end border-dark p-2" value={markdown} style={theme==="dark"?editorDarkText: editorLightText} onChange={(event)=> updateMarkdown(event.target.value)}/>
-                <div id="preview" className="w-50 h-100 overflow-auto border-start border-dark p-2" dangerouslySetInnerHTML={{__html:marked(markdown)}}></div>
+            <div className="w-100 m-0 d-flex flex-row shadow rounded-bottom" style={editorPrevStyle}> 
+                <textarea id="editor" className="textarea w-50 h-100 border-0 bg-transparent border-end border-dark p-2 flex-grow-1" value={markdown} style={ editorStyle} onChange={(event)=> updateMarkdown(event.target.value)}/>
+                <div id="preview" className="w-50 h-100 overflow-auto border-start border-dark p-2 flex-grow-3" style={prevStyle} dangerouslySetInnerHTML={{__html:marked(markdown)}}/>
             </div>
         </div>
     );
